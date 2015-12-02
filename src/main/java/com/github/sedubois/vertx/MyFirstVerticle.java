9063d11debce9f00c84dd0c8b02a7d2c0880e25e
Sebastien Dubois <sdubois@protonmail.ch>
2015-12-02 07:58:28 +0200

    Add web router

--- src/main/java/com/github/sedubois/vertx/MyFirstVerticle.java
+++ src/main/java/com/github/sedubois/vertx/MyFirstVerticle.java
@@ -2,6 +2,8 @@ package com.github.sedubois.vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class MyFirstVerticle extends AbstractVerticle {
 
@@ -9,7 +11,7 @@ public class MyFirstVerticle extends AbstractVerticle {
   public void start(Future<Void> future) {
     vertx
         .createHttpServer()
-        .requestHandler(r -> r.response().end("<h1>Hello World!</h1>"))
+        .requestHandler(getRouter()::accept)
         .listen(8080, result -> {
           if (result.succeeded()) {
             future.complete();
@@ -17,5 +19,16 @@ public class MyFirstVerticle extends AbstractVerticle {
             future.fail(result.cause());
           }
         });
+  }
+
+  private Router getRouter() {
+    Router router = Router.router(vertx);
+    router.route("/").handler(routingContext -> {
+      HttpServerResponse response = routingContext.response();
+      response
+          .putHeader("content-type", "text/html")
+          .end("<h1>Hello from my first Vert.x 3 application</h1>");
+    });
+    return router;
   }
 }
