7118efcb3ddbbf276ff7da679bfeefdede4a6a26
Sebastien Dubois <sdubois@protonmail.ch>
2015-12-02 07:35:53 +0200

    Add unit test

---
+++ src/test/java/com/github/sedubois/vertx/MyFirstVerticleTest.java
@@ -0,0 +1,40 @@
+package com.github.sedubois.vertx;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+@RunWith(VertxUnitRunner.class)
+public class MyFirstVerticleTest {
+
+  private Vertx vertx = Vertx.vertx();
+
+  @Before
+  public void setUp(TestContext context) {
+    vertx.deployVerticle(MyFirstVerticle.class.getName(), context.asyncAssertSuccess());
+  }
+
+  @After
+  public void tearDown(TestContext context) {
+    vertx.close(context.asyncAssertSuccess());
+  }
+
+  @Test
+  public void repliesHello(TestContext context) {
+    final Async async = context.async();
+
+    vertx.createHttpClient().getNow(8080, "localhost", "/",
+     response -> {
+      response.handler(body -> {
+        context.assertTrue(body.toString().contains("Hello"));
+        async.complete();
+      });
+    });
+  }
+}
